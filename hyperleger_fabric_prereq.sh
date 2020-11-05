a875c504aaa75b8ae61414e89b8ab54fd73dabd0
Yellow Hoodie <coolmegh1995@gmail.com>
2020-11-05 12:51:34 +0000

    hyperleger_fabric
    create a prerequisites for linux install

---
+++ hyperleger_fabric_prereq.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get install git
+sudo apt-get install curl -y
+sudo apt-get install golang-go -y
+export GOPATH=$HOME/go
+export PATH=$PATH:$GOPATH/bin
+sudo apt-get install nodejs -y
+sudo apt-get install npm -y
+sudo apt-get install python -y
+sudo apt-get install docker -y
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
+sudo apt-get install docker-compose -y
+sudo apt-get upgrade
